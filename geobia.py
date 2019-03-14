63399d1037dfb7ca6dea181c2cf23237a8f1937e
mikecampagna <msc94@drexel.edu>
2019-03-14 13:42:28 -0400

    Added sample images

--- geobia.py
+++ geobia.py
@@ -6,33 +6,61 @@ import os
 # from rsgislib.segmentation import segutils
 # from rios import ratapplier
 
+os.getcwd()
+# Go to the directory you want to output the segmentation files
+os.chdir(r'T:\WilliamPenn_Share\EDS\BARE_HILLS\DATA\SPATIAL\segout_03')
+
 print(np.__version__)
 print('Available applications: ')
-print (str( otb.Registry.GetAvailableApplications()))
+apps = list( otb.Registry.GetAvailableApplications())
+for app in apps:
+    print(app)
 
+print('\nAvailable parameters for LargeScaleMeanShift: ')
 LargeScaleMeanShift = otb.Registry.CreateApplication('LargeScaleMeanShift')
-print (LargeScaleMeanShift.GetParametersKeys())
+params = list(LargeScaleMeanShift.GetParametersKeys())
+for param in params:
+    print(param)
 
 # 'in', 'spatialr', 'ranger', 'minsize', 'tilesizex', 'tilesizey', 'mode',
 # 'mode.vector.imfield', 'mode.vector.out', 'mode.raster.out', 'cleanup',
 # 'ram', 'inxml', 'outxml'
 
+# All of these parameters can be edited based on the computer in use and desired segmentation
+
 inras = r'T:\WilliamPenn_Share\EDS\BARE_HILLS\DATA\SPATIAL\barehills6in_prj_s.tif'
-spatialr = str(5)
-ranger = str(15)
-minsize = str(10)
-outshp = r'T:\WilliamPenn_Share\EDS\BARE_HILLS\DATA\SPATIAL\segout_02\seg_merged_02.shp'
+spatialr = 10   # Changing this parameter will effect run-time
+ranger = 40.00
+minsize = 40
+cleanup = 'True'
+ram = 2000
+outshp = r'T:\WilliamPenn_Share\EDS\BARE_HILLS\DATA\SPATIAL\segout_03\seg_merged_03.shp'
 
 LargeScaleMeanShift.SetParameterString("in", inras)
-LargeScaleMeanShift.SetParameterString("spatialr", spatialr)
-LargeScaleMeanShift.SetParameterString("ranger", ranger)
-LargeScaleMeanShift.SetParameterString("minsize", minsize)
+LargeScaleMeanShift.SetParameterInt("spatialr", spatialr)
+LargeScaleMeanShift.SetParameterFloat("ranger", ranger)
+LargeScaleMeanShift.SetParameterInt("minsize", minsize)
+LargeScaleMeanShift.SetParameterString("cleanup", cleanup)
+LargeScaleMeanShift.SetParameterInt("ram", ram)
 LargeScaleMeanShift.SetParameterString("mode.vector.out", outshp)
 
 start_time = time.clock()
 LargeScaleMeanShift.ExecuteAndWriteOutput()
 print("OTB operation took:")
-print(time.clock() - start_time, "seconds")
+print((time.clock() - start_time)/60, "minutes")
+
+# You may want to alter a raster band to increase or decrease it's weight
+
+# gdal
+
+
+
+
+
+
+
+
+
 
 
 
