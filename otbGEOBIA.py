d36bf303c7a458295715e8a457d51965191307ba
mikecampagna <msc94@drexel.edu>
2019-03-14 15:56:36 -0400

    Added K Means clustering method in R

--- otbGEOBIA.py
+++ otbGEOBIA.py
@@ -10,7 +10,8 @@ import os
 
 os.getcwd()
 # Go to the directory you want to output the segmentation files
-os.chdir(r'T:\WilliamPenn_Share\EDS\BARE_HILLS\DATA\SPATIAL\segout_03')
+os.mkdir(r'T:\WilliamPenn_Share\EDS\BARE_HILLS\DATA\SPATIAL\segout_04')
+os.chdir(r'T:\WilliamPenn_Share\EDS\BARE_HILLS\DATA\SPATIAL\segout_04')
 
 print(np.__version__)
 print('Available applications: ')
@@ -29,14 +30,17 @@ for param in params:
 # 'ram', 'inxml', 'outxml'
 
 # All of these parameters can be edited based on the computer in use and desired segmentation
+# spatialr and minsize are in pixels (0.25 sqft)
+# ranger is in radiometric units
+# ram is in MB
 
 inras = r'T:\WilliamPenn_Share\EDS\BARE_HILLS\DATA\SPATIAL\barehills6in_prj_s.tif'
-spatialr = 10   # Changing this parameter will effect run-time
-ranger = 40.00
-minsize = 40
+spatialr = 4   # Changing this parameter will effect run-time, higher means more avg.ing -> more time
+ranger = 15.00
+minsize = 4
 cleanup = 'True'
 ram = 2000
-outshp = r'T:\WilliamPenn_Share\EDS\BARE_HILLS\DATA\SPATIAL\segout_03\seg_merged_03.shp'
+outshp = r'T:\WilliamPenn_Share\EDS\BARE_HILLS\DATA\SPATIAL\segout_04\seg_merged_04.shp'
 
 LargeScaleMeanShift.SetParameterString("in", inras)
 LargeScaleMeanShift.SetParameterInt("spatialr", spatialr)
@@ -49,7 +53,7 @@ LargeScaleMeanShift.SetParameterString("mode.vector.out", outshp)
 start_time = time.clock()
 LargeScaleMeanShift.ExecuteAndWriteOutput()
 print("OTB operation took:")
-print((time.clock() - start_time)/60, "minutes")
+print(round((time.clock() - start_time)/60, 2), "minutes")
 
 
 
