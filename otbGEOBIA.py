9f13b9e14ad75e4cdb05a3d74947d2868e2e3101
mikecampagna <msc94@drexel.edu>
2019-03-15 10:36:40 -0400

    Added a log file for inputs and a cleanup of extra .tif files

--- otbGEOBIA.py
+++ otbGEOBIA.py
@@ -4,16 +4,18 @@
 
 import numpy as np
 import time
+import datetime
 import otbApplication as otb
 import os
+from path import Path
 
 
+runNum = '05'
 os.getcwd()
 # Go to the directory you want to output the segmentation files
-os.mkdir(r'T:\WilliamPenn_Share\EDS\BARE_HILLS\DATA\SPATIAL\segout_04')
-os.chdir(r'T:\WilliamPenn_Share\EDS\BARE_HILLS\DATA\SPATIAL\segout_04')
+os.mkdir(r'T:\WilliamPenn_Share\EDS\BARE_HILLS\DATA\SPATIAL\segout_'+runNum)
+os.chdir(r'T:\WilliamPenn_Share\EDS\BARE_HILLS\DATA\SPATIAL\segout_'+runNum)
 
-print(np.__version__)
 print('Available applications: ')
 apps = list( otb.Registry.GetAvailableApplications())
 for app in apps:
@@ -25,10 +27,6 @@ params = list(LargeScaleMeanShift.GetParametersKeys())
 for param in params:
     print(param)
 
-# 'in', 'spatialr', 'ranger', 'minsize', 'tilesizex', 'tilesizey', 'mode',
-# 'mode.vector.imfield', 'mode.vector.out', 'mode.raster.out', 'cleanup',
-# 'ram', 'inxml', 'outxml'
-
 # All of these parameters can be edited based on the computer in use and desired segmentation
 # spatialr and minsize are in pixels (0.25 sqft)
 # ranger is in radiometric units
@@ -37,11 +35,12 @@ for param in params:
 inras = r'T:\WilliamPenn_Share\EDS\BARE_HILLS\DATA\SPATIAL\barehills6in_prj_s.tif'
 spatialr = 4   # Changing this parameter will effect run-time, higher means more avg.ing -> more time
 ranger = 15.00
-minsize = 4
+minsize = 12
 cleanup = 'True'
 ram = 2000
-outshp = r'T:\WilliamPenn_Share\EDS\BARE_HILLS\DATA\SPATIAL\segout_04\seg_merged_04.shp'
+outshp = r'T:\WilliamPenn_Share\EDS\BARE_HILLS\DATA\SPATIAL\segout_'+runNum+'\seg_merged_'+runNum+'.shp'
 
+# Set the parameters for the application
 LargeScaleMeanShift.SetParameterString("in", inras)
 LargeScaleMeanShift.SetParameterInt("spatialr", spatialr)
 LargeScaleMeanShift.SetParameterFloat("ranger", ranger)
@@ -50,10 +49,33 @@ LargeScaleMeanShift.SetParameterString("cleanup", cleanup)
 LargeScaleMeanShift.SetParameterInt("ram", ram)
 LargeScaleMeanShift.SetParameterString("mode.vector.out", outshp)
 
+# Create a log file of the inputs used in this run in case we forget
+t = datetime.datetime.now()
+myFile = open('Inputs_'+runNum+'.txt', 'w')
+myFile.writelines('Run on {0}/{1}/{2} at {3}:{4}:{5}{6}'.format(t.year,t.month,t.day,t.hour,t.minute,t.second,'\n'))
+myFile.writelines('{0} = {1}{2}'.format('inras',inras,'\n'))
+myFile.writelines('{0} = {1}{2}'.format('spatialr',spatialr,'\n'))
+myFile.writelines('{0} = {1}{2}'.format('ranger',ranger,'\n'))
+myFile.writelines('{0} = {1}{2}'.format('minsize',minsize,'\n'))
+myFile.writelines('{0} = {1}{2}'.format('cleanup',cleanup,'\n'))
+myFile.writelines('{0} = {1}{2}'.format('ram',ram,'\n'))
+myFile.writelines('{0} = {1}{2}'.format('outshp',outshp,'\n'))
+
+# Run the OTB Algorithm
 start_time = time.clock()
 LargeScaleMeanShift.ExecuteAndWriteOutput()
 print("OTB operation took:")
 print(round((time.clock() - start_time)/60, 2), "minutes")
+
+myFile.writelines('{0}OTB operation took {1} minutes'.format('\n',round((time.clock() - start_time)/60, 2)))
+myFile.close()
+
+# Cleanup the extra raster files created by the application
+d = Path(os.getcwd())
+clean = d.walkfiles('*.tif')
+for file in clean:
+    file.remove()
+
 
 
 
